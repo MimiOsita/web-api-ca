9a4404fbbcd9c01a8e35eef839dd471aa49519b5
MimiOsita <20097663@mail.wit.ie>
2025-12-21 22:29:01 +0000

    added imports and routes in api/movies/index.js

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { getMovies } from '../tmdb-api'; 
+import { getUpcomingMovies } from '../tmdb-api';
+import { getTopRatedMovies } from '../tmdb-api';
 
 
 const router = express.Router();
@@ -10,6 +12,16 @@ const router = express.Router();
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
+router.get('/top_rated', asyncHandler(async (req, res) => {
+    const topRatedMovies = await getTopRatedMovies();
+    res.status(200).json(topRatedMovies);
 }));
 
 
