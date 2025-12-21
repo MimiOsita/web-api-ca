5d0747da6264fab1deb7401c7c1cc8705a7411e3
MimiOsita <20097663@mail.wit.ie>
2025-12-21 20:16:53 +0000

    Added authContext class

---
+++ react-movies/src/contexts/authContext.jsx
@@ -0,0 +1,49 @@
+import { useState, createContext } from "react";
+import { login, signup } from "../api/movies-api";
+
+export const AuthContext = createContext(null); 
+
+const AuthContextProvider = (props) => {
+    const existingToken = localStorage.getItem("token");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [authToken, setAuthToken] = useState(existingToken);
+    const [userName, setUserName] = useState("");
+
+    const setToken = (data) => {
+        localStorage.setItem("token", data);
+        setAuthToken(data);
+    };
+
+    const authenticate = async (userName, password) => {
+        const result = await login(userName, password);
+        if (result.token) {
+            setToken(result.token);
+            setIsAuthenticated(true);
+            setUserName(username);
+        }
+    };
+
+    const register = async (username, password) => {
+        const result = await signup(username, password);
+        return result.success;
+    };
+
+    const signout = () => {
+        setTimeout(() => setIsAuthenticated(false), 100);
+    };
+
+    return(
+        <AuthContext.Provider
+        value={{
+            isAuthenticated,
+            authenticate,
+            register,
+            signout,
+            userName,
+        }}
+      >
+        {props.children}
+      </AuthContext.Provider>  
+    );
+};
+export default AuthContextProvider;
